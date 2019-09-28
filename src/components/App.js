4af52351fc940d5abf122455bb2991aee3f6d9d6
Małgosia <maugonia@hotmai.com>
2019-09-28 14:59:25 +0200

    props-change

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import Result from './Result';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    value: ""
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render(){
+  return (
+    <div className="App">
+     <Form value={this.state.value} change={this.handleInputChange}/>
+     <Result />
+    </div>
+  );
+}
+}
+
+export default App;
