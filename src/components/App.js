fded6e19db48f524ca6a4ba44f6688a692973791
Małgosia <maugonia@hotmai.com>
2019-09-29 14:57:20 +0200

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,97 @@ import Form from './Form';
 import Result from './Result';
 import './App.css';
 
+// Klucz do API
+const APIKey = 'db21ea10a65548c2e76d7ad310b156e0'
+
+
 class App extends Component {
+	state = {
+		value: '',
+		date: '',
+		city: '',
+		longitude: '',
+		latitude: '',
+		sunrise: '',
+		sunset: '',
+		temp: '',
+		temp_min: '',
+		temp_max: '',
+		pressure: '',
+		wind: '',
+		humidity: '',
+		clouds: '',
+		err: false,
+	};
 
-  state = {
-    value: ""
-  }
+	handleInputChange = e => {
+		this.setState({
+			value: e.target.value,
+		});
+	};
 
-  handleInputChange = (e) => {
-    this.setState({
-      value: e.target.value
-    })
-  }
+	handleCitySubmit = e => {
+		e.preventDefault();
+		console.log('potwierdzony formularz');
+		const API =
+			`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}&units=metric`;
+	
+	fetch(API)
+		// .then(response => console.log(response))
+		// .catch(err => console.log(err))
+		.then(response => {
+			if(response.ok){
+				return response
+			}
+			throw Error("nie udało się")
+		})
+		.then(response => response.json())
+		// .then(data => console.log(data))
+		.then(data => {
 
-  render(){
-  return (
-    <div className="App">
-     <Form value={this.state.value} change={this.handleInputChange}/>
-     <Result />
-    </div>
-  );
-}
+			const time = new Date().toLocaleString()
+			this.setState(state => ({
+				err: false,
+
+				date: time,
+				city: state.value,
+				longitude: data.coord.lon,
+				latitude: data.coord.lat,
+				sunrise: data.sys.sunrise,
+				sunset: data.sys.sunset,
+				temp: data.main.temp,
+				temp_min: data.main.temp_min,
+				temp_max: data.main.temp_max,
+				pressure: data.main.pressure,
+				wind: data.wind.speed,
+				humidity: data.main.humidity,
+				clouds: data.clouds,
+			}))
+		})
+
+
+		.catch(err => {
+			console.log(err);
+			this.setState(state => ({
+				err: true,
+				city: this.state.value
+			}))
+		
+		})
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Form 
+				value={this.state.value} 
+				change={this.handleInputChange} 
+				submit={this.handleCitySubmit} 
+				/>
+				<Result weather={this.state}/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,14 @@
 import React from 'react';
 const  Form = props => {
     return (  
-        <form>
+        <form onSubmit={props.submit}>
             <input 
             type="text" 
             value={props.value}
             onChange={props.change}
             placeholder="Wpisz miasto"
             />
-            <button>Wyszukaj miasta</button>
+            <button>Wyszukaj miasto</button>
         </form>
     );
 }
