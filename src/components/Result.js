13cf071fae3497b469be92b150b18fc87e1ff957
Małgosia <maugonia@hotmai.com>
2019-10-04 23:36:42 +0200

    design-finish

--- src/components/Result.js
+++ src/components/Result.js
@@ -28,23 +28,60 @@ const Result = (props) => {
         const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
         content = (
             <div className="Resultat">
-                <h3>{city}</h3>
-                <h4><i class="fas fa-map-marker-alt"></i> {longitude, latitude}</h4>
-                <h4>Today is {date}</h4>
-                <img src={`http://openweathermap.org/img/wn/${weather_iconID}@2x.png`}/>
-                <h4><i class="fas fa-temperature-high"></i> Temperature: {Math.floor(temp)} &#176;C</h4>
-                <h5>min: {Math.floor(temp_min)} &#176;C max: {Math.floor(temp_max)} &#176;C</h5>
-                <h4><i class="fas fa-sun"></i> Sunrise: {sunriseTime}</h4>
-                <h4><i class="fas fa-moon"></i> Sunset: {sunsetTime}</h4>
-                <h4><i class="fas fa-compass"></i> Pressure: {pressure} hPa</h4>
-                <h4><i class="fas fa-wind"></i> Wind: {wind} km/h</h4>
-                <h4><i class="fas fa-tint"></i> Humidity: {humidity} %</h4>
+                <div className="City-time">
+                <h4>{city}</h4>
+                <h6>{date}</h6>
+                </div>
+
+                <div className = "Localisation">
+                <h6><i class="fas fa-map-marker-alt"></i> {longitude, latitude}</h6>
+                </div>
                
-                <h4><i class="fas fa-cloud-sun"></i> {weather_desc} </h4>
-                {/* <h4>Weather icon: {weather_iconID}</h4> */}
+                <div className = "Clouds">
+                <img src={`http://openweathermap.org/img/wn/${weather_iconID}@2x.png`}/>
+                <h3> {clouds} </h3>
+                </div>
+                
+                <div className = "Temp0">
+                    
+                <h1>{Math.floor(temp)}</h1>
+                <h2>&#176;</h2>
+                </div>
+               
+
+               <div className = "Temp">
+                <h4>{Math.floor(temp_min)} &#176;C</h4>
+                <hr class="Line"></hr>
+                <h4>{Math.floor(temp_max)} &#176;C</h4>
+                </div>
+
+            <div className ="Sun">
+                <div clasname = "sun-icon">
+                <h6><i class="fas fa-sun"></i> {sunriseTime}</h6>
+               </div>
+
+               <div className = "moon-icon">
+                <h6><i class="fas fa-moon"></i> {sunsetTime}</h6>
+                </div>
+            </div>
+            
+            <div className = "group1">
+                <div className = "wind-icon">
+                <h6><i class="fas fa-wind"></i></h6><h6>{wind} km/h</h6>
+                </div>
+                <div className= "pressure-icon">
+                <h6><i class="fas fa-compass"></i></h6><h6>{pressure} hPa</h6>
+                </div>
+                <div className = "humidity-icon">
+                <h6><i class="fas fa-tint"></i></h6><h6>{humidity} %</h6>
+                </div>
+            </div>
+
            
 
             </div>
+
+           
         )
     }
 
