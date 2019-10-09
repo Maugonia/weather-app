0a58f213d4ac1759e2ff97352baee808eb619c56
Małgosia <maugonia@hotmai.com>
2019-10-09 12:26:19 +0200

    Updates

---
+++ precache-manifest.3fb802796afa5b8cd2acfde1d333b8d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a80f4b80842680c187807284f0fd786",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "31eb3b5716e4fce8ff08",
+    "url": "/weather-app/static/css/main.32f4dc71.chunk.css"
+  },
+  {
+    "revision": "bcf3fedb7447b0140ab1",
+    "url": "/weather-app/static/js/2.5374431a.chunk.js"
+  },
+  {
+    "revision": "31eb3b5716e4fce8ff08",
+    "url": "/weather-app/static/js/main.863cd0f1.chunk.js"
+  },
+  {
+    "revision": "a29b1e04cb8370963446",
+    "url": "/weather-app/static/js/runtime-main.8e037509.js"
+  }
+]);
